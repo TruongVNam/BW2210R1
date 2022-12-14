67d5012a3ec704f35692f41f75f32d71f38751a0
TruongVNam <truongnam.css.yds@gmail.com>
2022-12-14 16:04:47 +0700

    Casestudy 1
    Done

--- Day 18 - Casestudy 1 - 08.12.2022/script.js
+++ Day 18 - Casestudy 1 - 08.12.2022/script.js
@@ -12,10 +12,80 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speedAsDuration: true
 });
 
+// var swiper = new Swiper(".testimonial-slider", {
+//     // spaceBetween:10,
+//     grabCursor:true,
+//     loop: true,
+//     centeredSlides:true,
+//     autoplay: {
+//         delay:9500,
+//         disableOnInteraction: false,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1024: {
+//         slidesPerView: 6,
+//       },
+//     },
+//   });
 
+var swiper = new Swiper(".feature-img", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+      delay:9500,
+      disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-
-
+var swiper = new Swiper(".testimonial-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+      delay:9500,
+      disableOnInteraction: false,
+  },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 
 
 
